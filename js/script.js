e6c354c6c3db14538a8312a4b15f7e22873d1a6a
Rafael <khachikyan38@gmail.com>
2021-12-20 00:42:38 +0300

    js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector(".js-button");
+const close = document.querySelector(".js-img");
+const menu = document.querySelector(".js-menu");
+const wrapper = document.querySelector(".js-wrapper");
+const body = document.body;
+
+hamburger.addEventListener("click", () => {
+  if (!menu.classList.contains("menu__active")) {
+    menu.classList.add("menu__active");
+    close.src = "images/icon-close.svg";
+    body.classList.add("body--is-fixed");
+  } else {
+    close.src = "images/icon-hamburger.svg";
+    menu.classList.remove("menu__active");
+    body.classList.remove("body--is-fixed");
+  }
+});
